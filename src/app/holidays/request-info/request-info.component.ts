56471e157c555db5f8bf20bac7545fc8eabd8cc2
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-08-18 13:22:01 +0200

    improve and add libraries

--- src/app/holidays/request-info/request-info.component.ts
+++ src/app/holidays/request-info/request-info.component.ts
@@ -1,23 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
 import { delay, map, switchMap } from 'rxjs/operators';
-import {AddressLookuper} from "../address-lookuper.service";
+import { AddressLookuper } from '../address-lookuper.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-request-info',
-  templateUrl: './request-info.component.html'
+  templateUrl: './request-info.component.html',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    NgIf,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class RequestInfoComponent implements OnInit {
   formGroup: FormGroup = this.formBuilder.group({
-    address: []
+    address: [],
   });
   title = 'Request More Information';
   @Input() address = '';
   submitter$ = new Subject<void>();
   lookupResult$: Observable<string> | undefined;
 
-  constructor(private formBuilder: FormBuilder, private lookuper: AddressLookuper) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private lookuper: AddressLookuper
+  ) {}
 
   ngOnInit(): void {
     if (this.address) {
@@ -26,7 +44,7 @@ export class RequestInfoComponent implements OnInit {
 
     this.lookupResult$ = this.submitter$.pipe(
       switchMap(() => this.lookuper.lookup(this.formGroup.value.address)),
-      delay(1000),
+      delay(0),
       map((found) => (found ? 'Brochure sent' : 'Address not found'))
     );
   }
